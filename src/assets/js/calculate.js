a0242b5a47d2329ede41c92527c60d6a75efe122
Paulo Sobrinho Ferreira <paulo.sobrinho@opabox.com.br>
2021-12-19 12:59:23 -0300

    added other test of the calculate

--- src/assets/js/calculate.js
+++ src/assets/js/calculate.js
@@ -1,8 +1,27 @@
 export default class Calculate {
   sum(number1, number2) {
-    return number1 + number2;
+    return Number(number1) + Number(number2);
   }
   subtract(number1, number2) {
-    return number1 - number2;
+    return Number(number1) - Number(number2);
   }
+  division(number1, number2) {
+    return Number(number1) / Number(number2);
+  }
+  multiply(number1, number2) {
+    return Number(number1) * Number(number2);
+  }
+  percentage(number1, number2) {
+    return (Number(number1) / 100) * Number(number2);
+  }
+
+  convertValue(number) {
+    
+    if (Object.is(+0, number)) return -0;
+
+    if (Object.is(-0, number)) return 0;
+
+    return Math.sign(Number(number)) ? -Math.abs(number) : Math.abs(number);
+  }
+
 }
--- src/test/calculate.spec.js
+++ src/test/calculate.spec.js
@@ -17,4 +17,30 @@ describe("Calculator", () => {
       expect(calculator.subtract(2, 2)).toEqual(0);
     });
   });
+
+  describe("Division(),  perform the division", () => {
+    it("should division 2 and 2 the result must be 1", () => {
+      expect(calculator.division(2, 2)).toEqual(1);
+    });
+  });
+
+  describe("Multiply(), perform the multiply", () => {
+    it("should multiply 2 and 2 the result must be 4", () => {
+      expect(calculator.multiply(2, 2)).toEqual(4);
+    });
+  });
+
+  describe("Percentage(), perform the percentage", () => {
+    it("should return a percentage of 10 over 100 which should be 10", () => {
+      expect(calculator.percentage(10, 100)).toEqual(10);
+    });
+  });
+
+  describe("convertValue()", () => {
+    it("must return the opposite value, that is, if it is positive, it must return negative, 100 must be -100", () => {
+      expect(calculator.convertValue(-0)).toEqual(0);
+    });
+  });
+
+
 });
